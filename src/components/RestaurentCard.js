9d1d8315ac2c3eb21702a089dde5ae4506c50d89
Akash151822 <sharmaakash45704@gmail.com>
2024-02-29 13:03:24 +0530

    episode 6

--- src/components/RestaurentCard.js
+++ src/components/RestaurentCard.js
@@ -1,7 +1,7 @@
 import { CDN_URL } from "../utils/constants"
 const RestaurentCard = (props) => {
     const { resData } = props
-    const { name, cloudinaryImageId, avgRating, costForTwo, deliveryTime, cuisines } = resData?.info
+    const { name, cloudinaryImageId, avgRating, costForTwo, sla, cuisines } = resData?.info
     return (
         <div className="res-card" >
             <img className="res-logo"
@@ -11,7 +11,7 @@ const RestaurentCard = (props) => {
             <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRating}</h4>
             <h4>{costForTwo}</h4>
-            <h4>{deliveryTime} minutes</h4>
+            <h4>{sla.slaString} minutes</h4>
         </div>
     )
 }
